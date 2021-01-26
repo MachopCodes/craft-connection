209686d8ac4ead8eacfee300517bd915c7b2fb49
machopcodes <william.andreae@uconn.edu>
2021-01-26 17:42:31 -0500

    reduced size of bodega logo

--- src/Modals/Argentina.js
+++ src/Modals/Argentina.js
@@ -13,10 +13,10 @@ const Argentina = () => {
       <Carousel fade={true}>
         <Carousel.Item className='fade-in'>
           <Modal.Body className='modal-body'>
-            <Col xs={6} lg={4}>
-              <img className="argentine wine" src={cab}/>
+            <Col xs="6" lg="4">
+              <img className="wine argentine" src={cab}/>
             </Col>
-            <Col xs={6} lg={8}>
+            <Col xs="6" lg="8">
               <Modal.Title className="title">Old Vines, Cabernet Sauvignon</Modal.Title>
               <p className="modal-text">
               A wine with an intense ruby red color. Concentrated juicy berry fruit aromas that lead into flavors of blackberry and red currant fruit. Well-balanced, smooth tannins. Hints of vanilla and cocoa appear throughout the finish.
@@ -26,10 +26,10 @@ const Argentina = () => {
         </Carousel.Item>
         <Carousel.Item>
           <Modal.Body className='modal-body'>
-            <Col xs={6} lg={4}>
+            <Col xs="6" lg="4">
               <img className="wine malb" src={malbec}/>
             </Col>
-            <Col xs={6} lg={8}>
+            <Col xs="6" lg="8">
               <Modal.Title className="title">Old Vines, Malbec</Modal.Title>
               <p className="modal-text">
               This is a dense wine, with an inky color and attractive cherry aromas. A fruity wine, with excellent body, intense tannins, and flavors of cherry and plum that intertwine with hints of cardamom and pepper. Prolonged, mouth filling finish.
